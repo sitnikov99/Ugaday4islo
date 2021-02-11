8ddef02d8a03880dd0fe8e723eed1597209e5df4
Stanislav <sit.stas14@mail.ru>
2021-02-11 16:57:02 +0900

    first commit

---
+++ app/src/main/java/com/example/ugaday4islo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ugaday4islo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView2);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button2);
+
+        guess = (int)(Math.random()*100);
+        gameFinished = false;
+    }
+
+    public void onClick(View v){
+        if (!gameFinished){
+            int inp=Integer.parseInt(etInput.getText().toString());
+            if(inp>guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp<guess)
+                tvInfo.setText(getResources().getString(R.string.behind));
+            if (inp==guess)
+            {
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinished = true;
+            }
+        }
+        else {
+            guess=(int)(Math.random()*100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished=false;
+        }
+        etInput.setText("");
+    }
+
+}
